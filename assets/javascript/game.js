084b5fc79e0edd84e1076758e55e78f2f0c41ff7
acarlie <ameliacarlie@gmail.com>
2019-07-05 11:53:26 -0400

    Added keypress events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
+var body = document.getElementsByTagName('body')[0];
 var container = document.getElementById('gameContainer');
 
+
 var colors = {
-    color: ["Seafoam", "Magenta", "Lavender", "Aqua", "Crimson", "Purple", "Lime", "Watermelon", "Goldenrod", "Turquoise"],
+    color: ["seafoam", "magenta", "lavender", "aqua", "crimson", "purple", "lime", "watermelon", "goldenrod", "turquoise"],
     colorHex: ["#73FFD3", "#FF28AD", "#CEADEB", "#01F4FF", "#FF013D", "#A853FF", "#B8FF59", "#FF6D84", "#FFC255", "#45C3C7"],
     random(){
         var random = Math.round(Math.random()*(this.color.length - 1));
@@ -10,7 +12,7 @@ var colors = {
         var colorHex = this.colorHex[random]; 
         var lettersArray = randomWord.split("", randomWordLength);
 
-        container.style.backgroundColor = colorHex;
+        body.style.backgroundColor = colorHex;
 
         for (var i = 0; i < randomWordLength; i++){
             var letterBox = document.createElement("span");
@@ -21,8 +23,8 @@ var colors = {
             var letterSpan = document.createElement("span");
             letterSpan.innerHTML = lettersArray[i];
             letterSpan.className = "letter";
+            letterSpan.setAttribute('data-letter', lettersArray[i]);
             letterBox.appendChild(letterSpan);
-
         }
         
     }
@@ -32,5 +34,17 @@ var colors = {
 colors.random();
 
 document.onkeypress=function(e){
-    console.log("key pressed");
+    var keyCode = e.keyCode;
+    var key = String.fromCharCode(keyCode);
+
+    document.querySelectorAll('[data-letter]').forEach(function(item){
+        var dataLetter = item.innerHTML;
+        if(key === dataLetter){
+            item.className = "letter visible";
+            console.log('worked');
+        } 
+
+    });
+
 }
+
