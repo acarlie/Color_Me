d7b06c5cb4903a19d3da44a81f77fdc12b473083
acarlie <ameliacarlie@gmail.com>
2019-07-05 18:24:55 -0400

    added sounds

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,43 @@
 var body = document.getElementsByTagName('body')[0];
 var container = document.getElementById('gameContainer');
+var soundCorrect = new Audio('assets/sounds/positive.mp3');
+var soundWrong = new Audio('assets/sounds/negative.mp3');
+var soundWin = new Audio('assets/sounds/word_cleared.mp3');
 
+soundCorrect.volume = .05;
 
 var colors = {
     color: ["seafoam", "magenta", "lavender", "aqua", "crimson", "purple", "lime", "watermelon", "goldenrod", "turquoise"],
     colorHex: ["#73FFD3", "#FF28AD", "#CEADEB", "#01F4FF", "#FF013D", "#A853FF", "#B8FF59", "#FF6D84", "#FFC255", "#45C3C7"],
-    random(){
-        var random = Math.round(Math.random()*(this.color.length - 1));
-        var randomWord = this.color[random];
-        var randomWordLength = this.color[random].length;
-        var colorHex = this.colorHex[random]; 
-        var lettersArray = randomWord.split("", randomWordLength);
-
+    random(x, y, z){
+   
+        var colorHex = this.colorHex[x]; 
         body.style.backgroundColor = colorHex;
 
-        for (var i = 0; i < randomWordLength; i++){
+        for (var i = 0; i < y; i++){
             var letterBox = document.createElement("span");
             letterBox.innerHTML = "";
             letterBox.className = "letterBox";
             container.appendChild(letterBox);
 
             var letterSpan = document.createElement("span");
-            letterSpan.innerHTML = lettersArray[i];
+            letterSpan.innerHTML = z[i];
             letterSpan.className = "letter";
-            letterSpan.setAttribute('data-letter', lettersArray[i]);
+            letterSpan.setAttribute('data-letter', z[i]);
             letterBox.appendChild(letterSpan);
         }
-        
+
+        console.log(z);
     }
     
 }
 
-colors.random();
+var random = Math.round(Math.random()*(colors.color.length - 1));
+var randomWord = colors.color[random];
+var randomWordLength = colors.color[random].length;
+var lettersArray = randomWord.split("", randomWordLength);
+
+colors.random(random, randomWordLength, lettersArray);
 
 document.onkeypress=function(e){
     var keyCode = e.keyCode;
@@ -39,12 +45,14 @@ document.onkeypress=function(e){
 
     document.querySelectorAll('[data-letter]').forEach(function(item){
         var dataLetter = item.innerHTML;
+
         if(key === dataLetter){
             item.className = "letter visible";
+            soundCorrect.play();
             console.log('worked');
-        } 
+        }
 
+        // console.log(wordArray);
     });
-
 }
 
