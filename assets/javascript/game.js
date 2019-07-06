cc69b316945d934268a8f223685d4e2d8078387a
acarlie <ameliacarlie@gmail.com>
2019-07-06 19:31:05 -0400

    Added reg exp to only allow for letter input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,123 @@
+//global variables
 var body = document.getElementsByTagName('body')[0];
 var container = document.getElementById('gameContainer');
+var guessCont = document.getElementById('guesses');
+var winCont = document.getElementById('wins');
+var guesses = 13;
+var wins = 0;
+
+guessCont.innerHTML = guesses;
+
+var random;
+var randomWord;
+var randomWordLength;
+var lettersArray;
+
+//sounds
 var soundCorrect = new Audio('assets/sounds/positive.mp3');
 var soundWrong = new Audio('assets/sounds/negative.mp3');
 var soundWin = new Audio('assets/sounds/word_cleared.mp3');
-
 soundCorrect.volume = .05;
 
+//colors obj
 var colors = {
     color: ["seafoam", "magenta", "lavender", "aqua", "crimson", "purple", "lime", "watermelon", "goldenrod", "turquoise"],
     colorHex: ["#73FFD3", "#FF28AD", "#CEADEB", "#01F4FF", "#FF013D", "#A853FF", "#B8FF59", "#FF6D84", "#FFC255", "#45C3C7"],
-    random(x, y, z){
-   
-        var colorHex = this.colorHex[x]; 
-        body.style.backgroundColor = colorHex;
+    letterBoxes(random, randomWordLength, lettersArray){
 
-        for (var i = 0; i < y; i++){
+        for (var i = 0; i < randomWordLength; i++){
             var letterBox = document.createElement("span");
             letterBox.innerHTML = "";
             letterBox.className = "letterBox";
             container.appendChild(letterBox);
 
             var letterSpan = document.createElement("span");
-            letterSpan.innerHTML = z[i];
+            letterSpan.innerHTML = lettersArray[i];
             letterSpan.className = "letter";
-            letterSpan.setAttribute('data-letter', z[i]);
+            letterSpan.setAttribute('data-letter', lettersArray[i]);
             letterBox.appendChild(letterSpan);
         }
 
-        console.log(z);
+    },
+    bgColor(random){
+        var colorHex = this.colorHex[random]; 
+        body.style.backgroundColor = colorHex;
     }
     
 }
 
-var random = Math.round(Math.random()*(colors.color.length - 1));
-var randomWord = colors.color[random];
-var randomWordLength = colors.color[random].length;
-var lettersArray = randomWord.split("", randomWordLength);
+// function randomize(){
+    //variables referncing colors
+    var random = Math.round(Math.random()*(colors.color.length - 1));
+    var randomWord = colors.color[random];
+    var randomWordLength = colors.color[random].length;
+    var lettersArray = randomWord.split("", randomWordLength);
 
-colors.random(random, randomWordLength, lettersArray);
+    //function call
+    colors.letterBoxes(random, randomWordLength, lettersArray);
+    colors.bgColor(random);
+// }
 
-document.onkeypress=function(e){
+
+var keyCount = 0;
+
+//keypress
+document.onkeyup = function(e){
+
+    // keyCount += 1;
+
+    // if (keyCount === 1){
+    //     randomize();
+    // }
+    // console.log(lettersArray);
+
     var keyCode = e.keyCode;
-    var key = String.fromCharCode(keyCode);
+    var key = String.fromCharCode(keyCode).toLowerCase();
+    var keyIndex = lettersArray.indexOf(key);
+    var visible = document.querySelectorAll('.visible').length;
 
-    document.querySelectorAll('[data-letter]').forEach(function(item){
-        var dataLetter = item.innerHTML;
+    if (key.match(/[a-z]/)){
+        // function game(){
 
-        if(key === dataLetter){
-            item.className = "letter visible";
+        // }
+
+        if (keyIndex > -1 && guesses > 0){
+
             soundCorrect.play();
-            console.log('worked');
+    
+            document.querySelectorAll('[data-letter]').forEach(function(item){
+                var dataLetter = item.innerHTML;
+        
+                if(key === dataLetter && keyIndex > -1){
+                    item.className = "letter visible";
+                } 
+            });
+    
+            //fix
+            if((randomWordLength - 1) === visible){
+                wins += 1;
+                console.log('you have ' + wins + " wins!");
+            }
+    
+        } else if (keyIndex < 0 && guesses > 0){
+    
+            soundWrong.play();
+    
+            guesses -= 1;
+            guessCont.innerHTML = guesses;
+    
+        } else if (guesses === 0){
+    
+            console.log('game over');
+    
+        } 
+
+        if((randomWordLength - 1) === visible){
+            wins += 1;
+            console.log('TEST TEST you have ' + wins + " wins!");
         }
 
-        // console.log(wordArray);
-    });
+    } 
+
 }
 
