841f831fc41dec4e2ad18c15c3ae14fdeb193357
acarlie <ameliacarlie@gmail.com>
2019-07-10 10:20:12 -0400

    Added styling, switched colors to array of objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,15 +5,30 @@ var game = {
     guessCont: document.getElementById('guesses'),
     winCont: document.getElementById('wins'),
     wrong: document.getElementById('wrong'),
+    headingText: document.getElementById('heading'),
     guesses: 10,
     wins: 0,
-    color: ["seafoam", "magenta", "lavender", "aqua", "crimson", "purple", "lime", "watermelon", "goldenrod", "turquoise"],
+    //turn into nested object
+    colorArray: [
+        {name: "seafoam", colorMain: "#BDFFF3", colorTwo: "#4AC29A"}, 
+        {name: "magenta", colorMain: "#f953c6", colorTwo: "#b91d73"}, 
+        {name: "lavender", colorMain: "#eaafc8", colorTwo: "#654ea3"}, 
+        {name: "aqua", colorMain: "#A6FFCB", colorTwo: "#12D8FA"}, 
+        {name: "purple", colorMain: "#E100FF", colorTwo: "#7F00FF"}, 
+        {name: "lime", colorMain: "#DCE35B", colorTwo: "#45B649"},
+        {name: "citrus", colorMain: "#FDC830", colorTwo: "#F37335"},
+        {name: "maroon", colorMain: "#DA4453", colorTwo: "#89216B"},
+        {name: "peach", colorMain: "#FFC371", colorTwo: "#FF5F6D"},
+        {name: "violet", colorMain: "#4776E6", colorTwo: "#8E54E9"}
+        ],
+    color: ["seafoam", "magenta", "lavender", "aqua", "crimson", "purple", "lime", "watermelon", "goldenrod", "turquoise"], //turn into nested object
     colorHex: ["#73FFD3", "#FF28AD", "#CEADEB", "#01F4FF", "#FF013D", "#A853FF", "#B8FF59", "#FF6D84", "#FFC255", "#45C3C7"],
     random: "",
     randomWord: "",
     randomWordLength: "",
     lettersArray: "",
     wrongLettersArray: [],
+    correctLettersArray: [],
     soundCorrect: 'assets/sounds/positive.mp3',
     soundWrong: 'assets/sounds/negative.mp3',
     soundWin: 'assets/sounds/word_cleared.mp3',
@@ -37,26 +52,36 @@ var game = {
             letterSpan.setAttribute('data-letter', lettersArray[i]);
             letterBox.appendChild(letterSpan);
         }
-
     },
     bgColor(random){
-        var colorHex = this.colorHex[random]; 
-        this.body.style.backgroundColor = colorHex;
+        var colorHex = random.colorMain; 
+        var colorHexTwo = random.colorTwo; 
+        this.body.style.background = 'linear-gradient(to right,' + colorHex + ',' + colorHexTwo + ')';
+    },
+    textShadow(random){
+        var colorHexTwo = random.colorTwo; 
+        this.headingText.style.textShadow = '2px 4px 6px ' + colorHexTwo;
+    },
+    generator(randomNum){
+        this.random = this.colorArray[randomNum];
+        this.randomWord = this.random.name;
+        this.randomWordLength = this.randomWord.length;
+        this.lettersArray = this.randomWord.split("", this.randomWordLength);
+        
+
+        this.letterBoxes(this.randomWordLength, this.lettersArray);
+        this.bgColor(this.random);
+        this.textShadow(this.random);
     },
     reset(randomNum){
         this.guesses = 10;
         this.guessCont.innerHTML = game.guesses;
         this.container.innerHTML = "";
         this.wrongLettersArray = [];
+        this.correctLettersArray = [];
         this.wrong.innerHTML = "";
 
-        this.random = randomNum;
-        this.randomWord = this.color[randomNum];
-        this.randomWordLength = this.color[randomNum].length;
-        this.lettersArray = this.randomWord.split("", this.randomWordLength);
-
-        this.letterBoxes(this.randomWordLength, this.lettersArray);
-        this.bgColor(randomNum);
+        this.generator(randomNum);
     },
     win(){
         this.wins += 1;
@@ -84,6 +109,10 @@ var game = {
     
 }
 
+console.log(game.colorArray[1].name);
+
+console.log(game.colorArray.length);
+
 window.onload = function(){
     var randomNum = Math.round(Math.random()*(game.color.length - 1));
     game.reset(randomNum);
@@ -95,11 +124,13 @@ document.onkeyup = function(e){
     var keyCode = e.keyCode;
     var key = String.fromCharCode(keyCode).toLowerCase();
     var keyIndex = game.lettersArray.indexOf(key);
+    var correctGuessesIndex = game.correctLettersArray.indexOf(key);
     var visible = document.querySelectorAll('.visible').length;
+    
 
     if (key.match(/[a-z]/)){
 
-        if (keyIndex > -1 && game.guesses > 0){
+        if (keyIndex > -1 && game.guesses > 0 && correctGuessesIndex === -1){
 
             game.sound(game.soundCorrect, .05);
 
@@ -107,6 +138,7 @@ document.onkeyup = function(e){
                 var dataLetter = item.innerHTML;
                 if(key === dataLetter && keyIndex > -1){
                     item.className = "letter visible";
+                    game.correctLettersArray.push(key);
                 } 
             });
 
