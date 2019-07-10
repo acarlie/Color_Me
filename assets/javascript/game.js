997277599fe06cea132280a81243ef128c4ae72d
acarlie <ameliacarlie@gmail.com>
2019-07-10 16:47:07 -0400

    Worked on styling, added game over screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-//colors obj
+//game obj
 var game = {
     body: document.getElementsByTagName('body')[0],
     container: document.getElementById('gameContainer'),
@@ -9,29 +9,30 @@ var game = {
     wordSpan: document.getElementById('letters'),
     loadWrap: document.getElementById('loaderWrapper'),
     loadText: document.getElementById('loadText'),
+    finalWrapper: document.getElementById('finalWrapper'),
+    grid: document.getElementById('grid'),
     guesses: 10,
     wins: 0,
-    //turn into nested object
     colorArray: [
-        {name: "seafoam", colorMain: "#BDFFF3", colorTwo: "#4AC29A"}, 
-        {name: "magenta", colorMain: "#f953c6", colorTwo: "#b91d73"}, 
-        {name: "lavender", colorMain: "#e1ceff", colorTwo: "#d277c8"}, 
-        {name: "aqua", colorMain: "#A6FFCB", colorTwo: "#12D8FA"}, 
-        {name: "purple", colorMain: "#E100FF", colorTwo: "#7F00FF"}, 
-        {name: "lime", colorMain: "#DCE35B", colorTwo: "#45B649"},
-        {name: "citrus", colorMain: "#FDC830", colorTwo: "#F37335"},
-        {name: "maroon", colorMain: "#DA4453", colorTwo: "#89216B"},
-        {name: "peach", colorMain: "#FFC371", colorTwo: "#FF5F6D"},
-        {name: "violet", colorMain: "#a415e9", colorTwo: "#4522ad"},
-        {name: "crimson", colorMain: "#e9152d", colorTwo: "#ad2249"},
-        {name: "bubblegum", colorMain: "#ff8ed0", colorTwo:"#e45acb"},
-        {name: "rose", colorMain: "#ffaddd", colorTwo: "#ff6e8b"},
-        {name: "avocado", colorMain: "#d9e45a", colorTwo: "#9db230"},
-        {name: "pumpkin", colorMain: "#ff9b39", colorTwo: "#e4582c"},
-        {name: "navy", colorMain: "#394483", colorTwo: "#10163a"},
-        {name: "turquoise", colorMain: "#63ecd3", colorTwo: "#15a1a2"}
+        {name: "seafoam", colorMain: "BDFFF3", colorTwo: "74,194,154"}, 
+        {name: "magenta", colorMain: "f953c6", colorTwo: "185,29,115"}, 
+        {name: "lavender", colorMain: "e1ceff", colorTwo: "210,119,200"},
+        {name: "aqua", colorMain: "A6FFCB", colorTwo: "18,216,250"}, 
+        {name: "purple", colorMain: "E100FF", colorTwo: "127,0,255"}, 
+        {name: "lime", colorMain: "DCE35B", colorTwo: "69,182,73"},
+        {name: "citrus", colorMain: "FDC830", colorTwo: "243,115,53"},
+        {name: "maroon", colorMain: "DA4453", colorTwo: "137,33,107"},
+        {name: "peach", colorMain: "FFC371", colorTwo: "255,95,109"},
+        {name: "violet", colorMain: "a415e9", colorTwo: "69,34,173"},
+        {name: "crimson", colorMain: "e9152d", colorTwo: "173,34,73"},
+        {name: "bubblegum", colorMain: "ff8ed0", colorTwo:"228,90,203"},
+        {name: "rose", colorMain: "ffaddd", colorTwo: "255,110,139"},
+        {name: "avocado", colorMain: "d9e45a", colorTwo: "157,178,48"},
+        {name: "pumpkin", colorMain: "ff9b39", colorTwo: "228,88,44"},
+        {name: "navy", colorMain: "394483", colorTwo: "16,22,58"},
+        {name: "turquoise", colorMain: "63ecd3", colorTwo: "21,161,162"}
         ],
-    loadingText: ["Getting things ready...", "Good news everyone! The page has loaded.", "One moment...", "Loading into the Matrix..."],
+    loadingText: ["Getting things ready...", "Loading into the Matrix...", "Good news everyone! The page has loaded.", "Firing up the hyperdrive...", "One moment..."],
     randomWordLength: "",
     lettersArray: "",
     wrongLettersArray: [],
@@ -53,28 +54,28 @@ var game = {
 
         for (var i = 0; i < randomWordLength; i++){
             var letterBox = document.createElement("span");
-            letterBox.innerHTML = "";
             letterBox.className = "letterBox";
             this.container.appendChild(letterBox);
 
             var letterSpan = document.createElement("span");
-            letterSpan.innerHTML = lettersArray[i];
+            letterSpan.innerHTML = "_";
             letterSpan.className = "letter";
             letterSpan.setAttribute('data-letter', lettersArray[i]);
             letterBox.appendChild(letterSpan);
         }
     },
-    bgColor(colorOne, colorTwo){
+    styles(colorOne, colorTwo, colorThree){
         this.body.style.background = 'linear-gradient(to right,' + colorOne + ',' + colorTwo + ')';
-    },
-    textShadow(colorTwo){
+        this.grid.style.boxShadow = '1px 3px 16px ' + colorTwo + ' inset, -2px -2px 8px rgba(255,255,255,.1), 2px 2px 2px rgba(255,255,255,.1) ';
         this.headingText.style.textShadow = '2px 4px 6px ' + colorTwo;
+        this.grid.style.backgroundColor = colorThree;
     },
     generator(randomNum){
         var random = this.colorArray[randomNum];
         var randomWord = random.name;
-        var colorHex = random.colorMain; 
-        var colorHexTwo = random.colorTwo;
+        var colorHex = "#" + random.colorMain; 
+        var colorHexTwo = "rgb(" + random.colorTwo + ")";
+        var colorHexTwoLight = "rgba(" + random.colorTwo + ", .25)";
 
         this.colorArray.splice(randomNum, 1);
 
@@ -82,8 +83,7 @@ var game = {
         this.lettersArray = randomWord.split("", this.randomWordLength);
         
         this.letterBoxes(this.randomWordLength, this.lettersArray);
-        this.bgColor(colorHex, colorHexTwo);
-        this.textShadow(colorHexTwo);
+        this.styles(colorHex, colorHexTwo, colorHexTwoLight);
     },
     reset(randomNum){
         this.guesses = 10;
@@ -102,7 +102,9 @@ var game = {
         this.winCont.innerHTML = this.wins;
     },
     lose(){
-        this.container.innerHTML = "game over, game will restart in 5 seconds";
+        this.finalWrapper.classList = "lose";
+        this.finalWrapper.innerHTML = '<h1 class="inset">Game Over</h1>';
+        console.log()
         this.sound(this.soundLose, .25);
         console.log('game over');
     },
@@ -127,11 +129,10 @@ window.onload = function(){
     var randomNum = Math.round(Math.random()*(game.colorArray.length - 1));
     game.reset(randomNum);
     game.loadWrap.className = 'loaded';
+    game.loadText.classList = "fadeIn inset";
 }
 
-var keyCount = 0;
-
-document.onkeyup = function(e){
+document.onkeydown = function(e){
     var keyCode = e.keyCode;
     var key = String.fromCharCode(keyCode).toLowerCase();
     var keyIndex = game.lettersArray.indexOf(key);
@@ -142,11 +143,12 @@ document.onkeyup = function(e){
 
         if (keyIndex > -1 && game.guesses > 0 && correctGuessesIndex === -1){
 
-            game.sound(game.soundCorrect, .05);
+            game.sound(game.soundCorrect, .025);
 
             document.querySelectorAll('[data-letter]').forEach(function(item){
-                var dataLetter = item.innerHTML;
+                var dataLetter = item.getAttribute("data-letter");
                 if(key === dataLetter && keyIndex > -1){
+                    item.innerHTML = dataLetter;
                     item.className = "letter visible";
                     game.correctLettersArray.push(key);
                 } 
@@ -160,6 +162,7 @@ document.onkeyup = function(e){
                     var randomNum = Math.round(Math.random()*(game.colorArray.length - 1));
                     setTimeout(function(){game.reset(randomNum);}, 1000); 
                 } else{
+
                     //write for if someone goes through all options
                 }
                
