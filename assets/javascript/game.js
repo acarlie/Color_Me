9772bba1197f05a104b9439d89252e6a86ee2394
acarlie <ameliacarlie@gmail.com>
2019-07-11 21:07:33 -0400

    Added more colors to colorArray

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,12 +12,20 @@ var game = {
     finalWrapper: document.getElementById('finalWrapper'),
     resultsCont: document.getElementById('results'),
     border: document.querySelectorAll('.border'),
+    soundCorrect: 'assets/sounds/positive.mp3',
+    soundWrong: 'assets/sounds/negative.mp3',
+    soundWin: 'assets/sounds/word_cleared.mp3',
+    soundLose: 'assets/sounds/lose.mp3',
     guesses: 10,
     wins: 0,
-    gameTotalWins: 0,
     gamePlay: true,
+    randomWord: "",
+    randomWordLength: "",
+    lettersArray: "",
+    wrongLettersArray: [],
+    correctLettersArray: [],
     colorArray: [
-        {name: "seafoam", colorMain: "BDFFF3", colorTwo: "74,194,154"}, 
+        {name: "seafoam", colorMain: "BDFFF3", colorTwo: "74,194,154"},
         {name: "magenta", colorMain: "f953c6", colorTwo: "185,29,115"},
         {name: "lavender", colorMain: "e1ceff", colorTwo: "210,119,200"},
         {name: "aqua", colorMain: "A6FFCB", colorTwo: "18,216,250"}, 
@@ -32,19 +40,16 @@ var game = {
         {name: "rose", colorMain: "ffaddd", colorTwo: "255,110,139"},
         {name: "avocado", colorMain: "d9e45a", colorTwo: "157,178,48"},
         {name: "orange", colorMain: "ff9b39", colorTwo: "228,88,44"},
-        {name: "turquoise", colorMain: "63ecd3", colorTwo: "21,161,162"}
+        {name: "turquoise", colorMain: "63ecd3", colorTwo: "21,161,162"},
+        {name: "gold", colorMain: "ffdf7a", colorTwo: "255,162,27"},
+        {name: "sky", colorMain: "13eeff", colorTwo: "122,167,255"},
+        {name: "chartreuse", colorMain: "ffef5f", colorTwo: "170,194,52"},
+        {name: "storm", colorMain: "bebebe", colorTwo: "62,88,124"},
+        {name: "melon", colorMain: "baffd8", colorTwo: "0,255,165"},
+        {name: "cherry", colorMain: "#ef473a", colorTwo: "203,45,62"}
         ],
     colorArrayCopy: [],
     loadingText: ["Getting things ready...", "Loading into the Matrix...", "Good news everyone! The page has loaded.", "Firing up the hyperdrive...", "One moment..."],
-    randomWord: "",
-    randomWordLength: "",
-    lettersArray: "",
-    wrongLettersArray: [],
-    correctLettersArray: [],
-    soundCorrect: 'assets/sounds/positive.mp3',
-    soundWrong: 'assets/sounds/negative.mp3',
-    soundWin: 'assets/sounds/word_cleared.mp3',
-    soundLose: 'assets/sounds/lose.mp3',
     sound(sound, volume){
         var newSound = new Audio(sound);
         newSound.volume = volume;
@@ -54,15 +59,13 @@ var game = {
         var random = Math.round(Math.random()*(game.loadingText.length - 1));
         this.loadText.innerHTML = this.loadingText[random];
     },
-    letters(randomWordLength, lettersArray){
-        for (var i = 0; i < randomWordLength; i++){
-
-            var letterSpan = document.createElement("span");
-            letterSpan.innerHTML = "_";
-            letterSpan.className = "letter";
-            letterSpan.setAttribute('data-letter', lettersArray[i]);
-            this.container.appendChild(letterSpan);
-
+    letters(length, arr){
+        for (var i = 0; i < length; i++){
+            var letter = document.createElement("span");
+            letter.innerHTML = "_";
+            letter.className = "letter";
+            letter.setAttribute('data-letter', arr[i]);
+            this.container.appendChild(letter);
         }
     },
     colorChange(color, change){
@@ -71,7 +74,6 @@ var game = {
         oldColorRGB.forEach(function(i){
             newColorRGB.push(Math.round(parseInt(i)*change));
         });
-
         return 'rgb(' + newColorRGB.toString() + ')';
     },
     styles(colorOne, colorTwo, colorThree, colorFour){
@@ -131,15 +133,17 @@ var game = {
         this.wins += 1;
         this.winCont.innerHTML = this.wins;
     },
+    finalScreen(classList, heading, additionalHTML){
+        this.finalWrapper.classList = 'fixed-wrap ' + classList;
+        this.finalWrapper.innerHTML = '<div class="container"><div><h1 class="inset">' + heading + '</h1><div class="outcome"><p><strong>Wins: </strong>' + this.wins + '</p>' + additionalHTML + '</div><h3 class="text-center">Press Any Key to Continue</h3></div></div>';
+    },
     finalWin(){
         this.wins += 1;
-        this.finalWrapper.classList = 'fixed-wrap win';
-        this.finalWrapper.innerHTML = '<div class="container"><div><h1 class="inset">Success!</h1><div class="outcome"><p><strong>Wins: </strong>' + this.wins + '</p></div><h3 class="text-center">Press Any Key to Continue</h3></div></div>';
+        this.finalScreen('win', 'Success!', "");
     },
-    lose(totalWins){
-        this.finalWrapper.classList = 'fixed-wrap lose';
-        this.finalWrapper.innerHTML = '<div class="container"><div><h1 class="inset">Game Over</h1><div class="outcome"><p><strong>Word was: </strong>' + this.randomWord + '<p><strong>Wins: </strong>' + totalWins + '</p></div><h3 class="text-center">Press Any Key to Continue</h3></div></div>';
+    lose(){
         this.sound(this.soundLose, .25);
+        this.finalScreen('lose', "Game Over",  '<p><strong>Word was: </strong>' + this.randomWord + '</p>')
     },
     wrongGuess(key){
         if (this.wrongLettersArray.indexOf(key) === -1){
@@ -151,7 +155,6 @@ var game = {
         } 
      
     }
-    
 }
 
 game.randomLoadText();
@@ -184,9 +187,10 @@ document.onkeydown = function(e){
     if (key.match(/[a-z]/) && game.gamePlay){
 
         if (keyIndex > -1 && game.guesses > 0 && correctGuessesIndex === -1){
+
             //guess is correct
             game.sound(game.soundCorrect, .025);
-
+            
             document.querySelectorAll('[data-letter]').forEach(function(item){
                 var dataLetter = item.getAttribute("data-letter");
                 if(key === dataLetter && keyIndex > -1){
@@ -196,8 +200,9 @@ document.onkeydown = function(e){
                 } 
             });
 
+            //word completed
             if((game.randomWordLength - 1) === correct){
-                //word completed
+        
                 game.sound(game.soundWin, .25);
                 setTimeout(function(){game.win();}, 1000);
 
@@ -218,7 +223,7 @@ document.onkeydown = function(e){
 
         } else if (game.guesses === 0){
             //lose
-            game.lose(game.wins);
+            game.lose();
             game.gamePlay = false;
         } 
 
